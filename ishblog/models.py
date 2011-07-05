a10047bcfe58f7dcdd1a8988adee65f647a8e9b6
Seth Woodworth <seth@sethish.com>
2011-07-05 13:17:01 -0400

    setting up an app to store and render blog content

--- ishblog/models.py
+++ ishblog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+class Entry(models.Model):
+    title       = models.CharField(max_length=140)
+    snip        = models.CharField(max_length=140)
+    slug        = models.SlugField()
+    pub_date    = models.DateTimeField('Date Published')
+    body        = models.TextField()
+    published   = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name        = 'entry'
+        verbose_name_plural = 'entries'
+
+    def get_absolute_url(self):
+        return u'/blog/0%d/%02d/%s/' % (self.pub_date.year, self.pub_date.month, self.slug)
+
+    def __unicode__(self):
+        return u'%s' % (self.title,)
--- ishblog/views.py
+++ ishblog/views.py
@@ -1,1 +1,15 @@
-# Create your views here.
+from markdown import markdown
+from django.shortcuts import get_object_or_404, render_to_response
+
+from ishblog.models import Entry
+
+def entry(request, year, month, day, slug):
+    entry = get_object_or_404(Entry, slug=slug,
+            pub_date__year=year, pub_date__month=month, pub_date__day=day,)
+
+    return render_to_response('blog/entry.html', {'entry': entry})
+
+def list(request):
+    entries = Entry.objects.filter(published=True).order_by('-pub_date').all()
+
+    return render_to_response('blog/list.html', {'entries': entries } )
