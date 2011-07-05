966b97716d2ea43305403c0e8c29618b0628aaa0
Seth Woodworth <seth@sethish.com>
2011-07-05 18:23:05 -0400

    methods to load blog content from sub folders

--- ishblog/models.py
+++ ishblog/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-import datetime
 
 class Entry(models.Model):
     # loaded via yaml file
--- ishblog/views.py
+++ ishblog/views.py
@@ -19,19 +19,33 @@ def list(request):
     return render_to_response('blog/list.html', {'entries': entries } )
 
 def load():
-    years = ['02011']
+    """
+    Load a series of markdown and yaml files from content folders. YAML is for
+    blogpost metadata and markdown files are blogpost content.
+    """
+    years = ['02011'] # only one year of content for now
+    # TODO: use this in a blog-wide yaml config file, can generate 1996-2010(c)
     for year in years:
         folder = './content/blog/' + year
-        files = [ospath.splitext(f)[0] for f in os.listdir(folder)
+        files = [os.path.splitext(f)[0] for f in os.listdir(folder)]
         for file in files:
-            metadata    = open(file + '.yaml', 'r')
-            post        = open(file + '.md', 'r')
+            meta_file   = open(file + '.yaml', 'r')
+            metadata    = yaml.load(meta_file)
+            meta_file.close()
+
+            post_file   = open(file + '.md', 'r')
+            post        = post_file.read()
+            post_file.close()
+
             e = Entry()
 
             # Metadata
             e.title     = file
             e.slug      = slugify(file)
             e.pub_date  = metadata['pub_date']
-            # TODO: [
-
-
+            # Post
+            e.body      = post
+            e.snip      = post[:139]            # 140 characters of fun
+            # TODO: implement django-tagging here
+            e.save()
+    pass
