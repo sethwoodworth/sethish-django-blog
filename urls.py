1574f4254b839de92bc51c398f156c1d3368ad3c
Seth Woodworth <seth@sethish.com>
2011-07-06 15:43:27 -0400

    making static files work with urls

--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+import settings
 
 from django.contrib import admin
 admin.autodiscover()
@@ -9,4 +10,5 @@ urlpatterns = patterns('',
 
     #url(r'^projects/', include('djblog.projects.urls')),
     #url(r'^rss/', include('djblog.rss.urls')),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 )
